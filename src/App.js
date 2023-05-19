00b216ae311928b9c0362905d3ef135da6cdfee0
Dean <gomidean@gmail.com>
2023-05-19 13:47:20 +0900

    feat: vercel analytics package

--- src/App.js
+++ src/App.js
@@ -18,6 +18,9 @@ import SecretWord from "./scenes/secretword";
 // import Geography from "./scenes/geography";
 // import Calendar from "./scenes/calendar";
 
+import { inject } from "@vercel/analytics/*";
+
+inject();
 
 function App() {
   const [theme, colorMode] = useMode();
